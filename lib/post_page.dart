051763a908e048667fe30be7658fe09d4224986c
David Torres <davidatorres@cpp.edu>
2025-01-15 17:41:15 -0800

    added circular progress indicator when post button is pressed

--- lib/post_page.dart
+++ lib/post_page.dart
@@ -22,6 +22,7 @@ class _PostPageState extends State<PostPage> {
   String? _currentCityState;
   String? _currentCoordinates;
   final TextEditingController _commentController = TextEditingController();
+  bool _isLoading = false; // To control loading state
 
   final Location _location = Location();
   late final GoogleGeocodingApi _geocodingApi;
@@ -98,6 +99,10 @@ class _PostPageState extends State<PostPage> {
   }
 
   Future<void> _uploadPost() async {
+    setState(() {
+      _isLoading = true; // Show loading indicator
+    });
+
     try {
       final user = FirebaseAuth.instance.currentUser;
 
@@ -105,6 +110,9 @@ class _PostPageState extends State<PostPage> {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('User not logged in')),
         );
+        setState(() {
+          _isLoading = false; // Hide loading indicator
+        });
         return;
       }
 
@@ -139,14 +147,19 @@ class _PostPageState extends State<PostPage> {
         _images.clear();
         _fetchLocationAndDetails();
         _commentController.clear(); // Clear the comment TextField
+        _isLoading = false; // Hide loading indicator
       });
     } catch (e) {
       print("Error uploading post: $e");
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Failed to upload post: $e')),
       );
+    } finally {
+      setState(() {
+        _isLoading = false; // Hide loading indicator
+      });
     }
-  }
+  } // _uploadPost
 
   @override
   Widget build(BuildContext context) {
@@ -160,132 +173,143 @@ class _PostPageState extends State<PostPage> {
           ),
         ),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: _images.isEmpty
-                ? const Center(
-              child: Text(
-                'No images added yet',
-                style: TextStyle(fontSize: 18, color: Colors.grey),
+          Column(
+            children: [
+              Expanded(
+                child: _images.isEmpty
+                    ? const Center(
+                  child: Text(
+                    'No images added yet',
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                )
+                    : GridView.builder(
+                  padding: const EdgeInsets.all(8.0),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                  ),
+                  itemCount: _images.length,
+                  itemBuilder: (context, index) {
+                    return Image.file(
+                      _images[index],
+                      fit: BoxFit.cover,
+                    );
+                  },
+                ),
               ),
-            )
-                : GridView.builder(
-              padding: const EdgeInsets.all(8.0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 8.0,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: const TextStyle(fontSize: 16, color: Colors.black),
+                        children: [
+                          const TextSpan(
+                            text: "Date: ",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: _currentDate ?? 'Loading...',
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        style: const TextStyle(fontSize: 16, color: Colors.black),
+                        children: [
+                          const TextSpan(
+                            text: "City, State: ",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: _currentCityState ?? 'Loading...',
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        style: const TextStyle(fontSize: 16, color: Colors.black),
+                        children: [
+                          const TextSpan(
+                            text: "Coordinates: ",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: _currentCoordinates ?? 'Loading...',
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Text(
+                      "Comment:",
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 4),
+                    TextField(
+                      controller: _commentController,
+                      maxLength: 2200,
+                      maxLines: 3,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        hintText: 'Add a comment...',
+                        hintStyle: TextStyle(
+                          color:
+                          Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              itemCount: _images.length,
-              itemBuilder: (context, index) {
-                return Image.file(
-                  _images[index],
-                  fit: BoxFit.cover,
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                RichText(
-                  text: TextSpan(
-                    style: const TextStyle(fontSize: 16, color: Colors.black),
-                    children: [
-                      const TextSpan(
-                        text: "Date: ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      TextSpan(
-                        text: _currentDate ?? 'Loading...',
-                      ),
-                    ],
-                  ),
-                ),
-                RichText(
-                  text: TextSpan(
-                    style: const TextStyle(fontSize: 16, color: Colors.black),
-                    children: [
-                      const TextSpan(
-                        text: "City, State: ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      TextSpan(
-                        text: _currentCityState ?? 'Loading...',
-                      ),
-                    ],
-                  ),
-                ),
-                RichText(
-                  text: TextSpan(
-                    style: const TextStyle(fontSize: 16, color: Colors.black),
-                    children: [
-                      const TextSpan(
-                        text: "Coordinates: ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      TextSpan(
-                        text: _currentCoordinates ?? 'Loading...',
-                      ),
-                    ],
-                  ),
-                ),
-                const Text(
-                  "Comment:",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 4),
-                TextField(
-                  controller: _commentController,
-                  maxLength: 2200,
-                  maxLines: 3,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: 'Add a comment...',
-                    hintStyle: TextStyle(
-                      color:
-                      Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: _pickImagesFromGallery,
+                      icon: const Icon(Icons.image),
+                      label: const Text('Gallery'),
                     ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: _pickImagesFromGallery,
-                  icon: const Icon(Icons.image),
-                  label: const Text('Gallery'),
-                ),
-                ElevatedButton.icon(
-                  onPressed: _pickImageFromCamera,
-                  icon: const Icon(Icons.camera_alt),
-                  label: const Text('Camera'),
-                ),
-                ElevatedButton.icon(
-                  onPressed: _uploadPost,
-                  icon: const Icon(Icons.post_add_outlined),
-                  label: const Text('Post'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey[300], // Darker background color
-                    elevation: 5, // Drop shadow elevation
-                    shadowColor: Colors.grey[600], // Shadow color
-                    side: BorderSide(
-                      color: Colors.grey[700]!, // Darker outline color
-                      width: 1, // Thickness of the outline
+                    ElevatedButton.icon(
+                      onPressed: _pickImageFromCamera,
+                      icon: const Icon(Icons.camera_alt),
+                      label: const Text('Camera'),
                     ),
-                  ),
+                    ElevatedButton.icon(
+                      onPressed: _uploadPost,
+                      icon: const Icon(Icons.post_add_outlined),
+                      label: const Text('Post'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[300], // Darker background color
+                        elevation: 5, // Drop shadow elevation
+                        shadowColor: Colors.grey[600], // Shadow color
+                        side: BorderSide(
+                          color: Colors.grey[700]!, // Darker outline color
+                          width: 1, // Thickness of the outline
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
+          if (_isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
         ],
       ),
     );
